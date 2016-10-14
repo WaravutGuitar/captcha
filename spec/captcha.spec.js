62d98e7886748ece0aeed119472160d6f3ac05b8
Waravut Woravut <guitar15057@outlook.com>
2016-10-14 21:42:59 +0700

    7 + FIVE

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,4 +1,3 @@
-
 function Captcha(pa , op , left , right)
 {
   this.generate = function(){
@@ -7,7 +6,8 @@ function Captcha(pa , op , left , right)
 }
 function operator(Op){
 	this.toString = function(){
-		if(Op === 1) {return '+';}
+		if(Op === 1) {
+      return '+';}
 		else if(Op === 2) {
       return '-';}
 		else if(Op === 3) {
@@ -16,3 +16,63 @@ function operator(Op){
       return '/';}
 	}
 }
+function OperandL(pa,left){
+	this.toString = function(){
+		    if(pa === 1) {
+          return left;}
+        else if(pa ===2)
+          if(Left === 1){
+          return 'ONE';}
+  			else if(Left === 2){
+          return 'TWO';}
+  			else if(Left === 3){
+          return 'THREE';}
+  			else if(Left === 4){
+          return 'FOUR';}
+  			else if(Left === 5){
+          return 'FIVE';}
+  			else if(Left === 6){
+          return 'SIX';}
+  			else if(Left === 7){
+          return 'SEVEN';}
+  			else if(Left === 8){
+          return 'EIGHT';}
+  			else if(Left === 9){
+          return 'NINE';}
+	}
+}
+function OperandR(pa,right){
+	this.toString = function(){
+		if(pa === 2) {
+      return right;}
+		else if(pa === 1){
+			if(right === 0){
+        return 'ZERO';}
+			else if(right === 1){
+        return 'ONE';}
+			else if(right === 2){
+        return 'TWO';}
+			else if(right === 3){
+        return 'THREE';}
+			else if(right === 4){
+        return 'FOUR';}
+			else if(right === 5){
+        return 'FIVE';}
+			else if(right === 6){
+        return 'SIX';}
+			else if(right === 7){
+        return 'SEVEN';}
+			else if(right === 8){
+        return 'EIGHT';}
+			else if(right === 9){
+        return 'NINE';}
+		}
+	}
+}
+
+describe('Captcha' , function() {
+	it('should echo " + FIVE" when input (1,1,7,5)',function(){
+		let test = new Captcha(1,1,7,5);
+	 expect(test.generate()).toEqual('7 + FIVE');
+ 	});
+});
